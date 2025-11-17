7f66bc47508b0853bd96bc92314542baf325a114
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-11-17 10:04:44 +0600

    problem 7

--- solution.ts
+++ solution.ts
@@ -83,3 +83,26 @@ const printBookDetails = (book: Book): void => {
   );
 };
 
+const getUniqueValues = <T>(arr1: T[], arr2: T[]): T[] => {
+  const result: T[] = [];
+  const exists = (value: T): boolean => {
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] === value) return true;
+    }
+    return false;
+  };
+
+  for (let i = 0 ; i < arr1.length ; i++){
+    if(!exists(arr1[i])){
+      result.push(arr1[i])
+    }
+  }
+  for (let i = 0 ; i < arr2.length ; i++){
+     if(!exists(arr2[i])){
+      result.push(arr2[i])
+     }
+  }
+
+  return result;
+};
+
