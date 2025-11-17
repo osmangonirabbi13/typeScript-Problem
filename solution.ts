e5983ed9312d8d3e5f4609be7a1a2f8b9a8dd697
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-11-17 09:05:31 +0600

    problem 4

--- solution.ts
+++ solution.ts
@@ -32,3 +32,12 @@ class Person{
     }
 }
 
+type Item = {
+  title: string;
+  rating: number;
+};
+
+const filterByRating = (items: Item[]): Item[] => {
+  return items.filter(item => item.rating >= 4);
+};
+
